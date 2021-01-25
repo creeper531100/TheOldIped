90b08ab372117aee22d0df8ff729a12dc765ec29
a399555720 <47444528+a399555720@users.noreply.github.com>
2021-01-25 14:49:54 +0800

    新民彭于晏的新增頁面

--- app/src/main/java/com/example/thenewipad/page/MainActivity.java
+++ app/src/main/java/com/example/thenewipad/page/MainActivity.java
@@ -24,6 +24,7 @@ import com.example.thenewipad.function.JsonDataFormat;
 import com.example.thenewipad.page.busPageAdaper.BusPageInfo;
 import com.example.thenewipad.page.mainAdaper.CellularPage;
 import com.example.thenewipad.page.mainAdaper.HomePage;
+import com.example.thenewipad.page.mainAdaper.MapPage;
 import com.example.thenewipad.page.mainAdaper.PersonPage;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -118,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
                             selectedFragment = new CellularPage();
                             break;
                         case R.id.nav_setting:
-                            selectedFragment = new HomePage();
+                            selectedFragment = new MapPage();
                             break;
                     }
                     assert selectedFragment != null;
