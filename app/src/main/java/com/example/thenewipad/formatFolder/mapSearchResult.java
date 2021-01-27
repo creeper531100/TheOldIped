e0bdbe1cff8617cbfc1dd7ea0319ba4b5de90d54
a399555720 <47444528+a399555720@users.noreply.github.com>
2021-01-27 16:27:41 +0800

    地圖資訊

---
+++ app/src/main/java/com/example/thenewipad/formatFolder/mapSearchResult.java
@@ -0,0 +1,88 @@
+package com.example.thenewipad.formatFolder;
+
+public class mapSearchResult {
+
+    /**
+     * 確定病名 : 嚴重特殊傳染性肺炎
+     * 研判年份 : 2020
+     * 研判週別 : 10
+     * 縣市 : 空值
+     * 性別 : F
+     * 是否為境外移入 : 是
+     * 年齡層 : 70+
+     * 確定病例數 : 1
+     */
+
+    private String 確定病名;
+    private String 研判年份;
+    private String 研判週別;
+    private String 縣市;
+    private String 性別;
+    private String 是否為境外移入;
+    private String 年齡層;
+    private String 確定病例數;
+
+    public String get確定病名() {
+        return 確定病名;
+    }
+
+    public void set確定病名(String 確定病名) {
+        this.確定病名 = 確定病名;
+    }
+
+    public String get研判年份() {
+        return 研判年份;
+    }
+
+    public void set研判年份(String 研判年份) {
+        this.研判年份 = 研判年份;
+    }
+
+    public String get研判週別() {
+        return 研判週別;
+    }
+
+    public void set研判週別(String 研判週別) {
+        this.研判週別 = 研判週別;
+    }
+
+    public String get縣市() {
+        return 縣市;
+    }
+
+    public void set縣市(String 縣市) {
+        this.縣市 = 縣市;
+    }
+
+    public String get性別() {
+        return 性別;
+    }
+
+    public void set性別(String 性別) {
+        this.性別 = 性別;
+    }
+
+    public String get是否為境外移入() {
+        return 是否為境外移入;
+    }
+
+    public void set是否為境外移入(String 是否為境外移入) {
+        this.是否為境外移入 = 是否為境外移入;
+    }
+
+    public String get年齡層() {
+        return 年齡層;
+    }
+
+    public void set年齡層(String 年齡層) {
+        this.年齡層 = 年齡層;
+    }
+
+    public String get確定病例數() {
+        return 確定病例數;
+    }
+
+    public void set確定病例數(String 確定病例數) {
+        this.確定病例數 = 確定病例數;
+    }
+}
